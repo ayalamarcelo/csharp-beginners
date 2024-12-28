000fe7eb732f7c74d62e902cfa7bd7f10d2cad7f
Marcelo <marcelorubenayala@outlook.com>
2024-12-27 22:16:28 -0300

    Update folders :wrench:

---
+++ arithmetic-operators/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        int friends = 10;
+        // friends = friends + 2;
+        // friends += 2; // 8
+        // friends++; // 6
+        
+        // friends = friends - 1;
+        // friends -= 1;
+        // friends--;
+
+        // friends = friends * 2;
+        // friends *= 2;
+        // friends = friends / 2;
+        // friends /= 2;
+
+        int remainder = friends % 2;
+
+        Console.WriteLine(remainder);
+    }
+}
---
+++ math-class/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program {
+    public static void Main(string[] args) {
+
+        double x = 3.99;
+        double y = 5;
+
+        // double a = Math.Pow(x, 3); // base, exponent 3 x 3 x 3 Potencia
+        // double b = Math.Sqrt(x); // square root Raiz
+        // double c = Math.Abs(x); // Devuelve el valor absoluto sin signo
+        // double d = Math.Round(x); // Redondea hacia el más cercano
+        // double e = Math.Ceiling(x); // Redondea hacia arriba al entero cercano
+        // double f = Math.Floor(x); // Redondea hacia abajo
+        // double g = Math.Max(x, y); // Devuelve el valor Min
+        double h = Math.Min(x, y); // Devuelve el valor Max
+
+        Console.WriteLine(h);
+    }
+}
