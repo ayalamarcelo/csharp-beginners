39bf43dcc9a0285f5cda6252d25c06296bdbef45
Marcelo <marcelorubenayala@outlook.com>
2025-02-05 21:58:06 -0300

    Update directory

---
+++ practice-3/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program {
+    public static int busquedaSecuencial(int[] numbers, int num) {
+        bool encontrado = false;
+        for(int i = 0; i < numbers.Length; i++) {
+            if(numbers[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args) {
+        int[] temperatura = new int[6] {23, 34, 53, 3, 1, 3};
+
+        /*for(int i = 0; i < 6; i++) {
+            Console.WriteLine($"Introduzca el valor {i} del array");
+            temperatura[i] = int.Parse(Console.ReadLine());
+        }*/
+
+        for(int i = 0; i < temperatura.Length; i++) {
+            Console.WriteLine(temperatura[i]);
+        }
+
+        Console.WriteLine($"Se encuentra en la posición: {busquedaSecuencial(temperatura, 1)}");
+
+    }
+}
